f7b760d3b8b8a076bef9ce6b714913f36cb629ff
Lapitskyi <lapitskiy4@gmail.com>
2021-04-06 22:26:13 +0300

    vercel env

--- src/service/position.js
+++ src/service/position.js
@@ -3,7 +3,7 @@ import * as axios from "axios";
 
 export const positionAPI ={
     getPosition() {
-        return axios.get(`https://api.ipregistry.co/?key=${process.env.REACT_APP_VERCEL_ENV_API_KEY_POSITION}`)
+        return axios.get(`https://api.ipregistry.co/?key=${process.env.REACT_APP_API_KEY_POSITION}`)
             .then(response => {
                 return response.data
             })
