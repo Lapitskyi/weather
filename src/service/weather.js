8b20b6bd7eeec89fd57a5ac65dcf3f88567e0540
Lapitskyi <65563135+Lapitskyi@users.noreply.github.com>
2024-08-06 23:08:43 +0300

    Update weather.js

--- src/service/weather.js
+++ src/service/weather.js
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: `https://api.openweathermap.org/data/2.5/`,
     method: 'get',
     params: {
-        'appid': `${process.env.REACT_APP_API_KEY_WEATHER || 51dad89eefad388f12dc3e66208be5c9}`
+        'appid': "51dad89eefad388f12dc3e66208be5c9"
     },
 
 });
